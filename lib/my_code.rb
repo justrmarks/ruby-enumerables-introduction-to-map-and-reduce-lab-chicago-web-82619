5a47ae0f2b1b6b9b2772e04c3fed1f24def739ac
Justice Marks <her@shedaddy.com>
2019-07-24 20:11:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,13 +11,29 @@ def map_to_negativize(source_array)
   return result
 end
 
-=begin
-def map_to_no_change(source_array) do 
+
+def map_to_no_change(source_array)  
+  result = []
+   i=0
+   while i<source_array.length do 
+     result.push(source_array[i])
+     i+= 1
+   end
+   
+  return result
 end
 
-def map_to_double(source_array) do 
+
+def map_to_double(source_array)  
+  result = []
+   i=0
+   while i<source_array.length do 
+     result.push(source_array[i]*2)
+     i+= 1
+   end
+   
+  return result
 end
 
-def map_to_square(source_array) do
+def map_to_square(source_array) 
 end
-=end
