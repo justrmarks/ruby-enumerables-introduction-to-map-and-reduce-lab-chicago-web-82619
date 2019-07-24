56bcd8b7c706b118edbbbbadc3823790e2a25e6d
Justice Marks <her@shedaddy.com>
2019-07-24 20:17:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,7 +60,7 @@ def reduce_to_all_true(source_array)
   result = TRUE
   i=0
   while i<source_array.length do
-    result = result || !!source_array[i]
+    result = result && !!source_array[i]
   end
 end
 
